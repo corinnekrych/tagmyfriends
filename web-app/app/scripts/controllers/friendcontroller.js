3037f5d6965e6054c565bd20e53ff52815a99d03
fabricematrat <fabricematrat@gmail.com>
2013-02-20 16:57:14 +0100

    adding directive to refresh list

--- web-app/app/scripts/controllers/friendcontroller.js
+++ web-app/app/scripts/controllers/friendcontroller.js
@@ -2,10 +2,18 @@
 
 friend.controller('FriendCtrl', function ($scope, $http, friend) {
     $scope.friend = friend;
+    $scope.friends = {};
     $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+    $http.get('http://localhost:8080/tagmyfriends/friend/list').success(function(data) {
+        for (var i = 0; i < data.length; i++) {
+            var element = data[i];
+            $scope.friends[element.id] = element;
+        }
+    });
 
     var createElement = function () {
         resetForm('form-update-friend');
+        $scope.friend = {};
         $.mobile.changePage($('#section-show-friend'));
         $('#delete-friend').css('display', 'none');
     };
@@ -20,29 +28,31 @@ friend.controller('FriendCtrl', function ($scope, $http, friend) {
                 dateOrder:'mmD ddyy'
             });
         });
-        var div = $("#" + form);
-        div.find('input:text, input:hidden, input[type="number"], input:file, input:password').val('');
-        div.find('input:radio, input:checkbox').removeAttr('checked').removeAttr('selected');
     };
 
-    $scope.addFriend = function (item) {
-        //event.stopPropagation();
+    $scope.addFriend = function (event) {
+        event.stopPropagation();
         //$('#form-update-friend').validationEngine('hide');
         //$('#form-update-friend').validationEngine({promptPosition:'bottomLeft'});
         createElement();
     };
 
-    $scope.create = function () {
-        var foo = $scope.friend.firstname;
-        var friendMock = {version: "", class: "friend", id: "", lastname: "krych", firstname: "corinne", registrationDate: "2013-02-16T11:00:00.000Z"};
-        //var toJson = JSON.stringify(friendMock);
+    $scope.create = function (event) {
+        event.stopPropagation();
+        $scope.friend.registrationDate = $('#input-friend-registrationDate').scroller('getDate', true);
         var toJson = {friend:JSON.stringify($scope.friend)};
         toJson = $.param(toJson);
         $http.post("http://localhost:8080/tagmyfriends/friend/save", toJson)
-            .success(function(data, status, headers, config) {
-                $scope.data = data;
-            }).error(function(data, status, headers, config) {
-                $scope.status = status;
+            .success(function(item, status, headers, config) {
+                if (item.errors || item.message) {
+                    return false;
+                }
+                $scope.friends[item.id] = item;
+
+                $.mobile.changePage($('#section-list-friend'));
+            }).error(function(item, status, headers, config) {
+
+
             });
     };
 
