2b02cf84984ee2558e1034b0a5f5145de2e22f0b
corinnekrych <corinnekrych@gmail.com>
2013-03-03 22:40:25 +0100

    test

--- web-app/test/spec/controllers/friendcontroller.js
+++ web-app/test/spec/controllers/friendcontroller.js
@@ -19,4 +19,5 @@ describe('Controller: FriendCtrl', function() {
   it('should attach a list of awesomeThings to the scope', function() {
     expect(scope.awesomeThings.length).toBe(3);
   });
+
 });
